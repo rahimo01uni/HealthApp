82971cce279b2c82078c7b23348bc422857ebe4a
Alimerdan <rahimo01@ads.uni-passau.de>
2020-05-25 18:13:49 +0200

    added bottom navigation and recycler

--- app/src/main/java/com/health/healthapp/MainActivity.java
+++ app/src/main/java/com/health/healthapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.health.healthapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,7 +12,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +21,12 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_progress, R.id.navigation_group,R.id.navigation_reminder)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
--- app/src/main/java/com/health/healthapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/health/healthapp/ui/home/HomeFragment.java
@@ -11,25 +11,52 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.tabs.TabLayout;
 import com.health.healthapp.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    TabLayout tabLayout;
+    TextView text;
+    RecyclerView  all_view;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        tabLayout=root.findViewById(R.id.home_tab);
+        text=root.findViewById(R.id.home);
+        all_view=root.findViewById(R.id.recyclerView);
+        all_view.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onTabSelected(TabLayout.Tab tab) {
+                  text.setText(""+tab.getText());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
+
+
+
+
         return root;
+    }
+    void Init(){
+
+
     }
 }
